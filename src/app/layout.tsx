ccfbf0393d8b1e61657530cfbce9065b31317f82
Brian Felix <fbrian100@gmail.com>
2026-02-10 09:57:17 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,22 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          id="clicky-script"
-          strategy="afterInteractive"
           src="https://static.getclicky.com/js"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var clicky_site_ids = clicky_site_ids || [];
-              clicky_site_ids.push(101500850);
-              (function() {
-                var s = document.createElement('script');
-                s.type = 'text/javascript';
-                s.async = true;
-                s.src = '//static.getclicky.com/js';
-                ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
-              })();
-            `,
-          }}
+          strategy="afterInteractive"
+          data-id="101500850"
         />
         <noscript>
           <p>
